54ec9d9d0053c34cd9b30450270c20369b0c0237
Thomas Soto <thomas.soto@usm.cl>
2024-05-12 15:54:25 -0400

    form changed

---
+++ datasheet.js
@@ -0,0 +1,37 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    
+    // Obtener los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var empresa = document.getElementById("empresa").value;
+    var numero = document.getElementById("numero").value;
+    var mensaje = document.getElementById("mensaje").value;
+    
+    // Aquí puedes validar los datos del formulario si es necesario
+    
+    // Enviar los datos a Google Sheets utilizando Fetch API
+    fetch('https://script.google.com/macros/s/AKfycbzYGPNxGiNAXdBXrEzHngmyVYCzTxsyA3VL0gH2_-kVtvOpcwRkJ9XXJBNZDygUoRsb/exec', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nombre: nombre,
+        email: email,
+        empresa: empresa,
+        numero: numero,
+        mensaje: mensaje
+      })
+    })
+    .then(response => {
+      // Aquí puedes manejar la respuesta, por ejemplo, mostrar un mensaje de éxito o error
+      console.log('¡Datos enviados con éxito!');
+    })
+    .catch(error => {
+      // Manejar cualquier error que pueda ocurrir
+      console.error('Error al enviar los datos:', error);
+    });
+  });
+  
