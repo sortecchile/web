89555843bb224d5a5fceaaa036079db76c37d9d3
Nicolás Castellón <nicolascastellon@MacBook-Air-de-Nicolas.local>
2024-08-30 18:22:40 -0400

    Deploy Next.js app to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/web', // Reemplaza 'nombre-del-repositorio' con el nombre de tu repositorio
   };
   
   export default nextConfig;
-  
