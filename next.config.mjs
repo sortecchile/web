f6cfb3e79c33c35b2ebc0a87815ad5f496c4867e
Nicolás Castellón <nicolascastellon@MacBook-Air-de-Nicolas.local>
2024-08-30 20:54:53 -0400

    web esta si que si que si

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/web', // Reemplaza 'nombre-del-repositorio' con el nombre de tu repositorio
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
