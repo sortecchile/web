e9832478e79e4d07a8f3f932b8e533489ccbacdc
Nicolás Castellón <nicolascastellon@MacBook-Air-de-Nicolas.local>
2024-08-31 00:30:50 -0400

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Exportar como sitio estático
+  images: {
+    unoptimized: true, // Evita optimizaciones de imágenes en tiempo de construcción
+  },
+  basePath: '/web', // Reemplaza <REPO_NAME> con el nombre de tu repositorio
+};
 
 export default nextConfig;
