0b10b920b4f91776ed632d9eaf94d8dcb0e3aa3a
Nicolás Castellón <nicolascastellon@MacBook-Air-de-Nicolas.local>
2024-09-02 19:39:04 -0400

    Variables de desarrollo y prod

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: isProd ? 'export' : undefined, // Solo 'export' en producción
+  distDir: isProd ? 'dist' : '.next', // Usar 'dist' en producción y '.next' en desarrollo
   images: {
-    unoptimized: true,
+    unoptimized: true, // Mantén esto igual para ambos entornos
   },
-  assetPrefix: 'https://www.miido.cl/', // Asegúrate de usar tu dominio personalizado
+  assetPrefix: isProd ? 'https://www.miido.cl/' : '', // Usar el prefijo solo en producción
 }
 
 module.exports = nextConfig;
