b564358e63ca5e92fd5b3b3a31ec2da6a7952744
Nicolás Castellón <nicolascastellon@MacBook-Air-de-Nicolas.local>
2024-09-03 18:36:36 -0400

    lineas

--- app/page.tsx
+++ app/page.tsx
@@ -578,8 +578,11 @@ export default function Component() {
               </div>
             </div>
           </div>
+          
         </section>
+        <MultiWaveAudio />
         <section className="w-full py-12 md:py-24 lg:py-32">
+    
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12" style={{ color: '#2F3D44' }}>Principales beneficios</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
